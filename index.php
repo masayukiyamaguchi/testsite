965e5c63301cd6169f7927bf1b497e15b250af7e
unknown <echofct37@gmail.com>
2021-04-10 21:57:05 +0900

    Create index.html

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <title>testsite</title>
+    <link rel="stylesheet" href="stylesheet.css">   
+</head>
+<body>
+
+<footer>
+<div class="inside">
+
+
+<header>
+    <img  class="logo" src="http://localhost/testsite/image/logo.jpg">
+    <img class="media" src="http://localhost/testsite/image/icon_facebook.gif">
+    <img class="media" src="http://localhost/testsite/image/icon_instagram.gif">
+</header>
+
+<div class="menu">
+<p>
+<ul class="nav">
+<li>About</li>
+<li>Works</li>
+<li>Blog</li>
+<li>Contact</li>
+</ul>
+</p>
+</div>
+
+</div>
+</footer>
+
+<div class="main">
+<img src="http://localhost/testsite/image/top_main.jpg">
+</div>
+
+<footer>
+<div class="inside">
+
+<div>
+<h1>PLAY JOB</h1>
+</div>
+
+<div class="contents">
+
+    <div class="contents-item">
+        <h2>ナイト</h2>
+        <p>Paladin</p>
+        <img class="jobicon" src="http://localhost/testsite/image/PLD.png">
+        <p>ウルダハの近衛兵団「銀冑団」の騎士たちは、忠誠を誓う王家を守るため、長い歴史の中で技を磨き上げてきた。白銀の甲冑で身を固め、大盾と騎士剣を手に戦いに身を投じる彼らは、剣術だけではなく護衛術にも長け、主君の命を断固として守り抜く。</p>
+    </div>
+
+    <div class="contents-item">
+        <h2>竜騎士</h2>
+        <p>Dragoon</p>
+        <img class="jobicon" src="http://localhost/testsite/image/DRG.png">
+        <p>竜を屠る者……「竜騎士」。建国以来、ドラゴン族との果て無き戦いを続ける宗教都市「イシュガルド」において、その言葉が持つ意味は限りなく重い。ドラゴン族を倒す。その目的のためだけに発展した槍術は、硬き鱗に隠されたドラゴン族の急所を狙うため、独特の跳躍攻撃を生み出した。</p>
+    </div>
+
+    <div class="contents-item">
+        <h2>白魔道士</h2>
+        <p>White Mage</p>
+        <img class="jobicon" src="http://localhost/testsite/image/WHM.png">
+        <p>癒やしと浄化の力として編み出されながらも、人々の尽きぬ欲望のために乱用され、大災厄を引き起こした歴史を持つ「白魔法」。一度は、禁忌として封じられたが、後世、「グリダニア」の「角尊」たちの手で復元され、密かに継承されてきた。</p>
+    </div>
+
+</div>
+
+<p></p>
+
+<div>
+<h1>STRATEGY</h1>
+</div>
+
+<div class="contents_strategy">
+
+    <div class="contents_strategy_item">
+        <img class="strategy" src="http://localhost/testsite/image/samneil20210410.jpg">
+    </div>
+
+    <div class="contents_strategy_item">
+        <img class="strategy" src="http://localhost/testsite/image/20201212samneil.jpg">
+    </div>
+
+    <div class="contents_strategy_item">
+        <img class="strategy" src="http://localhost/testsite/image/20201228samneil.jpg">
+    </div>
+
+    <div class="contents_strategy_item">
+        <img class="strategy" src="http://localhost/testsite/image/20210111samneil.jpg">
+    </div>
+
+    <div class="contents_strategy_item">
+        <img class="strategy" src="http://localhost/testsite/image/20210301024.jpg">
+    </div>
+
+    <div class="contents_strategy_item">
+        <img class="strategy" src="http://localhost/testsite/image/samneil20210221.jpg">
+    </div>
+
+</div>   
+
+
+
+<div>
+<h1>BLOG</h1>
+</div>
+
+<div class="contents_blog">
+
+    <div class="contents_blog_item">
+        <img class="blog" src="http://localhost/testsite/image/ffxiv_20210207_024900_162.png">
+    </div>
+
+    <div class="contents_blog_item">
+        <img class="blog" src="http://localhost/testsite/image/ffxiv_20210111_160054_335.png">
+    </div>
+
+    <div class="contents_blog_item">
+        <img class="blog" src="http://localhost/testsite/image/ffxiv_20201108_022722_987.png">
+    </div>
+
+    <div class="contents_blog_item">
+        <img class="blog" src="http://localhost/testsite/image/ffxiv_20201023_174746_462.png">
+    </div>
+
+    <div class="contents_blog_item">
+        <img class="blog" src="http://localhost/testsite/image/ffxiv_20210205_023755_085.png">
+    </div>
+
+    <div class="contents_blog_item">
+        <img class="blog" src="http://localhost/testsite/image/ffxiv_20201212_131238_072.png">
+    </div>
+
+    
+
+</div>   
+
+
+<div class="footer">
+<p>
+<ul class="nav">
+<li>About</li>
+<li>Works</li>
+<li>Blog</li>
+<li>Contact</li>
+</ul>
+</p>
+</div>
+
+
+
+
+</div>
+</footer>
+
+
+
+</body>
+</html>
